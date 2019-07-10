43b4cd728ee7fc722cf4a2c331305be6b36fe093
SFromentarius <sarah.froment@wanadoo.fr>
2019-07-10 17:46:23 +0200

    Updates

---
+++ precache-manifest.4dbc0c1e19578e0752619eee6f286a39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e4c36f461fbecbb1ab55d6024025ad",
+    "url": "/daylight-backend/index.html"
+  },
+  {
+    "revision": "989123a15e48be5ce2b7",
+    "url": "/daylight-backend/static/css/main.b977301c.chunk.css"
+  },
+  {
+    "revision": "ce2419f963bf10fa0021",
+    "url": "/daylight-backend/static/js/2.7878a58e.chunk.js"
+  },
+  {
+    "revision": "989123a15e48be5ce2b7",
+    "url": "/daylight-backend/static/js/main.35072d0d.chunk.js"
+  },
+  {
+    "revision": "05ab91bb6eb0fda80bb8",
+    "url": "/daylight-backend/static/js/runtime~main.c8a31558.js"
+  }
+]);
