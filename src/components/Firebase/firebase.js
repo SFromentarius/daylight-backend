c67bab3bdcd7fc518f09f751dd6798b77dd236aa
SFromentarius <sarah.froment@wanadoo.fr>
2019-07-23 10:14:04 +0200

    fix(firebase): fix error

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -9,9 +9,12 @@ const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+  // projectId: process.env.REACT_APP_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  projectId: 'daylight-dev2',
+  storageBucket: 'daylight-dev2.appspot.com'
+  // messagingSenderId: '415713015602'
 };
 
 app.initializeApp(config);
